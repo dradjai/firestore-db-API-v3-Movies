c215bfe70b5fae4c3ab3b43685fbb06708c0e425
David Radjai <dradjai@yahoo.com>
2023-04-04 22:08:11 -0400

    SECOND commit updated APIs

--- src/MovieLibrary.js
+++ src/MovieLibrary.js
@@ -2,22 +2,24 @@ import { db } from "./dbConnect.js";
 
 
 // POST add movie
-export function addMovie (req, res) {
-  const newMovie = req.body
+export const addMovie = async (req, res) => {
+  const newMovie = req.body;
+  const result = await db.collection('Movies').add(newMovie)
+    .catch(error => console.log(error));
 
-  db.collection('Movies').add(newMovie)
-    .then(doc => res.status(201).send(`Movie added: ${doc.id}`))
-    .catch(error => res.status(500).send(err));
-
+  
+  res.status(201).send("Movie id: " + result.id);
 }
 
-// GET get all movies
-export async function getAllMovies (req, res) {
+export const getAllMovies = async (req, res) => {
   const collection = await db.collection('Movies').get()
-    .catch(error => res.status(500).send(error));
+    .catch(error => res.status(500).send(error))
 
-  const movieList = collection.docs.map(movie => ({...movie.data(), id: movie.id}));
-  res.status(201).send(movieList);
-
+    const movieList = collection.docs.map(movie => ({...movie.data(), id: movie.id}))
+    res.status(200).send(movieList);
 }
+
+
+
+
 
